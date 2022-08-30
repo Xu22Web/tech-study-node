212492270da8dda49893bdf4d3815dde9af3a5ca
Noah <xuzhengze2333@qq.com>
2022-08-30 19:13:34 +0800

    chore: update log style

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -20,14 +20,12 @@ export const renderUserData = async (page: pup.Page) => {
   // 用户信息
   const userInfo = await getUserInfo(page);
   if (userInfo) {
-    userProgress.succeed('获取用户信息成功!\n');
+    userProgress.succeed('获取用户信息成功!');
     userProgress.warn(`用户信息`);
-    userProgress.info(`昵称: ${chalk.yellow(userInfo.nick)}`);
+    userProgress.info(`昵称: ${chalk.yellow(userInfo.nick)}\n`);
     return userInfo;
-  } else {
-    userProgress.fail('获取用户信息失败!');
-    return;
   }
+  userProgress.fail('获取用户信息失败!');
 };
 
 /**
@@ -44,22 +42,19 @@ export const renderScoreData = async (page: pup.Page) => {
   // 总分
   const totalScore = await getTotalScore(page);
   if (score !== undefined && totalScore !== undefined) {
-    scoreProgress.succeed('获取积分信息成功!\n');
+    scoreProgress.succeed('获取积分信息成功!');
     scoreProgress.warn(`积分信息`);
     scoreProgress.info(
       `当天积分: ${chalk.yellow(score)} 分 | 总积分: ${chalk.yellow(
         totalScore
-      )} 分`
+      )} 分\n`
     );
-  } else {
-    scoreProgress.fail('正在获取积分信息失败!');
-    return;
+    return {
+      score,
+      totalScore,
+    };
   }
-
-  return {
-    score,
-    totalScore,
-  };
+  scoreProgress.fail('获取积分信息失败!');
 };
 /**
  * @description 渲染用户任务数据
@@ -72,13 +67,13 @@ export const renderTasksData = async (page: pup.Page) => {
   // 任务进度
   const taskList = await getTaskList(page);
   if (taskList && taskList.length) {
-    taskProgress.succeed(`获取${chalk.blueBright('任务进度')}成功!\n`);
+    taskProgress.succeed(`获取${chalk.blueBright('任务进度')}成功!`);
     taskProgress.warn(`任务进度`);
     taskProgress.info(`文章选读: ${chalk.yellow(taskList[0].rate)} %`);
     taskProgress.info(`视听学习: ${chalk.yellow(taskList[1].rate)} %`);
     taskProgress.info(`每日答题: ${chalk.yellow(taskList[2].rate)} %`);
     taskProgress.info(`每周答题: ${chalk.yellow(taskList[3].rate)} %`);
-    taskProgress.info(`专项练习: ${chalk.yellow(taskList[4].rate)} %`);
+    taskProgress.info(`专项练习: ${chalk.yellow(taskList[4].rate)} %\n`);
     return taskList;
   }
   taskProgress.fail('获取任务进度失败!');
