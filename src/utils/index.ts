13b75e890fb787d83e24a9636879dc32b6bbf56b
Noah <xuzhengze2333@qq.com>
2022-09-02 23:53:27 +0800

    feat: add more time format

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -44,7 +44,7 @@ const createModal = (options: ModalOptions) => {
     contentText = content;
   }
   // 日期
-  const dateTime = formatDate();
+  const dateTime = formatDateTime();
   // 类型html
   let typeHTML = '';
   if (type && type.length) {
@@ -205,9 +205,15 @@ export const pushModal = async (
 };
 
 /**
- * @description 格式化时间
+ * @description 格式化日期时间
+ * @param time
+ * @returns
+ * @example 
+ * formatDateTime() -> "2022-09-01 08:00:00"
+ * formatDateTime(new Date()) -> "2022-09-01 08:00:00"
+ * formatDateTime(Date.now()) -> "2022-09-01 08:00:00"
  */
-export const formatDate = (time: Date | string | number = Date.now()) => {
+export const formatDateTime = (time: Date | string | number = Date.now()) => {
   const date = new Date(time);
   const s = date.getSeconds();
   const min = date.getMinutes();
@@ -216,16 +222,69 @@ export const formatDate = (time: Date | string | number = Date.now()) => {
   const m = date.getMonth() + 1;
   const y = date.getFullYear();
   // 格式化时间
-  const formatNum = (num: number) => {
+  const formatDateNum = (num: number) => {
     return num < 10 ? `0${num}` : String(num);
   };
   // 日期
-  const dateText = [y, m, d].map(formatNum).join('-');
+  const dateText = [y, m, d].map(formatDateNum).join('-');
   // 时间
-  const timeText = [h, min, s].map(formatNum).join(':');
+  const timeText = [h, min, s].map(formatDateNum).join(':');
   // 日期时间
   const dateTimeText = `${dateText} ${timeText}`;
   return dateTimeText;
+};
+
+/**
+ * @description 格式化时间日期
+ * @param time
+ * @returns
+ * @example
+ * @example 
+ * formatDate() -> "2022-09-01"
+ * formatDate(new Date()) -> "2022-09-01"
+ * formatDate(Date.now()) -> "2022-09-01"
+ */
+export const formatDate = (time: Date | string | number = Date.now()) => {
+  const date = new Date(time);
+  const d = date.getDate();
+  const m = date.getMonth() + 1;
+  const y = date.getFullYear();
+  // 格式化时间
+  const formatDateNum = (num: number) => {
+    return num < 10 ? `0${num}` : String(num);
+  };
+  // 日期
+  const dateText = [y, m, d].map(formatDateNum).join('-');
+  return dateText;
+};
+
+/**
+ * @description 格式化时间
+ * @param time
+ * @returns
+ * @example 
+ * formatTime() -> "08:00:00"
+ * formatTime(new Date()) -> "08:00:00"
+ * formatTime(Date.now()) -> "08:00:00"
+ */
+export const formatTime = (time: Date | string | number = Date.now()) => {
+  const date = new Date(time);
+  const s = date.getSeconds();
+  const min = date.getMinutes();
+  const h = date.getHours();
+
+  // 时间
+  const timeText = [h, min, s].map(formatDateNum).join(':');
+  return timeText;
+};
+
+/**
+ * @description 格式化时间数字
+ * @param num
+ * @returns
+ */
+const formatDateNum = (num: number) => {
+  return num < 10 ? `0${num}` : String(num);
 };
 
 /**
