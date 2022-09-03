49d174dfde7b2ad677e2846908e262e778240f85
Noah <xuzhengze2333@qq.com>
2022-09-03 08:32:54 +0800

    chore: change log.clean to async function

--- src/controller/logs.ts
+++ src/controller/logs.ts
@@ -168,37 +168,42 @@ export class Log {
    */
   clean() {
     // 文件
-    const files = fs.readdirSync(logsPath);
-    // 存在文件
-    if (files.length) {
-      for (const i in files) {
-        // 文件名
-        const fileName = files[i];
-        // 获取后缀
-        const extname = path.extname(fileName);
-        if (extname === '.log') {
-          // 获取日志时间
-          const fileDate = fileName.substring(0, fileName.lastIndexOf('.'));
-          // 过去日期
-          const oldDate = new Date(fileDate);
-          // 当前时间
-          const newDate = new Date();
-          // 相差天数
-          const days =
-            (newDate.getTime() - oldDate.getTime()) / 1000 / 60 / 60 / 24;
-          // 相差超过间隔
-          if (days >= STUDY_CONFIG.logsAutoCleanInterval) {
-            // 日志文件路径
-            const filePath = path.join(logsPath, fileName);
-            // 删除日志
-            fs.unlinkSync(filePath);
-            this.start();
-            this.success(`日志: ${fileName}, 删除日志成功!`);
-            this.finish();
+    fs.readdir(logsPath, (err, files) => {
+      if (err) {
+        return;
+      }
+      // 存在文件
+      if (files.length) {
+        for (const i in files) {
+          // 文件名
+          const fileName = files[i];
+          // 获取后缀
+          const extname = path.extname(fileName);
+          if (extname === '.log') {
+            // 获取日志时间
+            const fileDate = fileName.substring(0, fileName.lastIndexOf('.'));
+            // 过去日期
+            const oldDate = new Date(fileDate);
+            // 当前时间
+            const newDate = new Date();
+            // 相差天数
+            const days =
+              (newDate.getTime() - oldDate.getTime()) / 1000 / 60 / 60 / 24;
+            // 相差超过间隔
+            if (days >= STUDY_CONFIG.logsAutoCleanInterval) {
+              // 日志文件路径
+              const filePath = path.join(logsPath, fileName);
+              // 删除日志
+              fs.unlink(filePath, () => {
+                this.start();
+                this.success(`日志: ${fileName}, 删除日志成功!`);
+                this.finish();
+              });
+            }
           }
         }
       }
-    }
+    });
   }
 }
 
