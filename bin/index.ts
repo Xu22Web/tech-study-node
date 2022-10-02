b1e0520d68ddf2ae8af7b1f57d0992facba24c75
Noah <1627295329@qq.com>
2022-10-02 11:31:47 +0800

    chore: modify init push

--- bin/index.ts
+++ bin/index.ts
@@ -1,13 +1,42 @@
-import pup from 'puppeteer-core';
-import schedule from 'node-schedule';
-import paser from 'cron-parser';
 import chalk from 'chalk';
+import schedule from 'node-schedule';
+import pup from 'puppeteer-core';
+import handleBrowser from '../src/app';
 import PUP_CONFIG from '../src/config/pup';
 import PUSH_CONFIG from '../src/config/push';
-import handleBrowser from '../src/app';
 import shared from '../src/shared';
-import { formatDateTime, getHighlightHTML } from '../src/utils';
-
+import { getHighlightHTML, getRestTaskList } from '../src/utils';
+/**
+ * @description 初始化
+ */
+const init = () => {
+  // 剩余任务
+  const rest = getRestTaskList(PUSH_CONFIG.list);
+  // 推送服务提示
+  shared.pushModalTips({
+    title: '服务提示',
+    content: [
+      '已运行定时任务!',
+      `今天剩余任务数: ${getHighlightHTML(rest.length)} 个`,
+      '剩余任务信息: ',
+      ...rest
+        .map((item) => {
+          return [
+            `用户: ${getHighlightHTML(item.nick)}`,
+            `时间: ${getHighlightHTML(item.timeText)}`,
+          ];
+        })
+        .flat(),
+    ],
+    type: 'info',
+  });
+  // 执行清除日志任务
+  schedule.scheduleJob('0 0 0 * * ?', () => {
+    // 清除日志
+    shared.log.autoClean();
+  });
+};
+init();
 // 主函数
 const main = async () => {
   shared.log.loading('正在打开浏览器...');
@@ -27,19 +56,6 @@ const main = async () => {
     throw e;
   }
 };
-// 推送服务提示
-shared.pushModalTips({
-  title: '服务提示',
-  content: '已运行定时任务!',
-  type: 'info',
-});
-
-// 执行清除日志任务
-schedule.scheduleJob('0 0 0 * * ?', () => {
-  // 清除日志
-  shared.log.autoClean();
-});
-
 // 定时任务
 PUSH_CONFIG.list.forEach((sendInfo, i) => {
   console.log(`${i + 1} / ${PUSH_CONFIG.list.length} 执行定时任务`);
@@ -67,29 +83,7 @@ PUSH_CONFIG.list.forEach((sendInfo, i) => {
       });
     }
     // 剩余任务
-    const rest = PUSH_CONFIG.list
-      .map((sendInfo) => {
-        // 任务时间
-        const time = paser.parseExpression(sendInfo.cron);
-        // 下次任务时间
-        const nextTime = time.next().toDate();
-        // 当前时间
-        const date = new Date();
-        // 当前任务是否结束
-        const done = !time.hasNext();
-        // 下个任务是否在今天
-        const isToday = !done && nextTime.getDate() === date.getDate();
-        return {
-          ...sendInfo,
-          isToday,
-          done,
-          timeText: formatDateTime(nextTime),
-          time: nextTime.getTime(),
-        };
-      })
-      .filter((sendInfo) => sendInfo.isToday)
-      .sort((a, b) => a.time - b.time);
-
+    const rest = getRestTaskList(PUSH_CONFIG.list);
     // 存在下次任务
     if (rest.length) {
       shared.log.warn('服务提示');
@@ -97,7 +91,7 @@ PUSH_CONFIG.list.forEach((sendInfo, i) => {
         `用户: ${chalk.yellow(sendInfo.nick)}, 定时任务已执行完毕!`
       );
       shared.log.info(`今天剩余任务数: ${chalk.yellow(rest.length)} 个`);
-      shared.log.info(`下次任务信息`);
+      shared.log.warn(`下次任务信息`);
       shared.log.info(`用户: ${chalk.yellow(rest[0].nick)}`);
       shared.log.info(`时间: ${chalk.yellow(rest[0].timeText)}`);
       // 推送服务提示
@@ -131,5 +125,3 @@ PUSH_CONFIG.list.forEach((sendInfo, i) => {
     shared.log.finish();
   });
 });
-
-// main();
